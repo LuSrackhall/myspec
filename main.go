36e29c1c368ef22f2ac34c0b03469e016886465b
LuSrackhall <3647637206@qq.com>
2026-06-16 21:49:45 +0800

    release: 版本升至 0.3.0
    精简 myspec-br 第 9 步输出，移除冗余收尾流程指引。
    新增 Windows 支持和 GitHub Actions 多平台二进制发布。

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ var embedFS embed.FS
 //go:embed openspec-version.txt
 var expectedOpenSpecVersion string
 
-const version = "0.2.0"
+const version = "0.3.0"
 
 var commands = map[string]struct {
 	run  func(args []string)

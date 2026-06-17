de15d4708cece1361af39832decc220813754898
LuSrackhall <3647637206@qq.com>
2026-06-17 14:00:30 +0800

    release: 版本升至 0.4.1
    myspec-merge Phase 1.5: 同步 main 后调用 myspec-verify 重新验证。

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ var embedFS embed.FS
 //go:embed openspec-version.txt
 var expectedOpenSpecVersion string
 
-const version = "0.4.0"
+const version = "0.4.1"
 
 var commands = map[string]struct {
 	run  func(args []string)

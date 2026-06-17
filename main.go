3b769e8c1e791e11a7631832ffd071d7647b7697
LuSrackhall <3647637206@qq.com>
2026-06-17 14:38:14 +0800

    release: 版本升至 0.4.2
    修复 update 全量更新 bug + 新技能动态嵌入。

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ var embedFS embed.FS
 //go:embed openspec-version.txt
 var expectedOpenSpecVersion string
 
-const version = "0.4.1"
+const version = "0.4.2"
 
 var commands = map[string]struct {
 	run  func(args []string)
